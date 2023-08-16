aa429c0406e7ebf50fd9ed2e4505aacc8c2e3205
yosefm@silverkeytech.com <235711af>
2023-08-16 23:32:05 +0300

    Enhance Controllers

---
+++ PizzaStore.API/Controllers/OrderController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PizzaStore.Core.Models;
+using PizzaStore.Core.Repositories;
+using Serilog;
+
+namespace PizzaStore.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class OrderController : Controller
+{
+    private readonly IOrderRepository _repository;
+
+    public OrderController(ILogger<OrderController> logger, IOrderRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet("get-orders")]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> GetAllOrders()
+    {
+        try
+        {
+            var orders = await _repository.GetAllOrdersAsync();
+
+            return Ok(orders);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+
+    [HttpPost("add-order")]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> AddOrder(OrderModel order)
+    {
+        try
+        {
+           var result = await _repository.AddOrder(order);
+
+            if (!result)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return Ok(result);
+        }
+        catch (Exception ex)
+        {
+            Log.Error(ex.Message);
+            return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
+        }
+    }
+}
---
+++ PizzaStore.API/Controllers/PizzaController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PizzaStore.Core.Models;
+using PizzaStore.Core.Repositories;
+
+namespace PizzaStore.API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class PizzaController : Controller
+{
+    private readonly IPizzaRepository _repository;
+
+    public PizzaController(IPizzaRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [HttpGet("pizzas")]
+    public async Task<IEnumerable<PizzaModel>> GetAllPizzas()
+    {
+        var pizzas = await _repository.GetAllPizzasAsync();
+        return pizzas;
+    }
+}
