ecc45da535f36f032efc841b5e66ae09cadca7a4
yosefm@silverkeytech.com <235711af>
2023-08-16 23:34:05 +0300

    Enhance workflow

--- PizzaStore/Program.cs
+++ PizzaStore/Program.cs
@@ -1,134 +1,137 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
-using PizzaStore.Models;
+using PizzaStore.Core.Models;
 using PizzaStore.Repositories;
 using Spectre.Console;
 
-namespace PizzaStore
+namespace PizzaStore;
+
+class Program
 {
-    class Program
+    static async Task Main()
     {
-        static async Task Main()
+        AnsiConsole.WriteLine("Welcome to pizza store!");
+
+        while (true)
         {
-            var pizzaRepository = new PizzaRepository();
-            var toppingRepository = new ToppingRepository();
-            var orderRepository = new OrderRepository();
-
-            await pizzaRepository.GetPizzaFromApi();
-            await toppingRepository.GetToppingFromApi();
-            await orderRepository.GetOrdersFromApi();
-
-            AnsiConsole.WriteLine("Welcome to pizza store!");
-
-            while (true)
-            {
-                var answer = AnsiConsole.Prompt(
-                    new TextPrompt<int>("Do you want to make a new order or check your old orders? 1 for new order, 2 for old orders, 0 to exit")
-                        .Validate(input =>
-                        {
-                            return input switch
-                            {
-                                < 0 => ValidationResult.Error("[red]Invalid input[/]"),
-                                > 2 => ValidationResult.Error("[red]Invalid input[/]"),
-                                _ => ValidationResult.Success(),
-                            };
-                        }));
-
-                var order = new Order();
-
-                if (answer == 1)
+            var answer = AnsiConsole.Prompt(new TextPrompt<int>("Do you want to make a new order or check your old orders? 1 for new order, 2 for old orders, 0 to exit")
+                .Validate(input =>
                 {
+                    return input switch
+                    {
+                        < 0 => ValidationResult.Error("[red]Invalid input[/]"),
+                        > 2 => ValidationResult.Error("[red]Invalid input[/]"),
+                        _ => ValidationResult.Success()
+                    };
+                })
+            );
+
+            var order = new OrderModel();
+
+            switch (answer)
+            {
+                case 1:
+                {
+                    var pizzas = await PizzaRepository.GetPizzaFromApi();
+                    var toppings = await ToppingRepository.GetToppingFromApi();
                     //Making an order
+                    var index = 1;
+
                     while (true)
                     {
-                        PizzaRepository.DisplayPizzaTable(pizzaRepository.PizzaList);
+                        PizzaRepository.DisplayPizzaTable(pizzas);
 
-                        var choosingPizza = AnsiConsole.Prompt(
-                            new TextPrompt<int>("Please choose your pizza")
-                                .Validate(pizzaId =>
-                                    pizzaRepository.GetPizzaById(pizzaId) == null
-                                        ? ValidationResult.Error("[red]Invalid input[/]")
-                                        : ValidationResult.Success()));
+                        var pizzaId = AnsiConsole.Prompt(new TextPrompt<int>("Please choose your pizza")
+                            .Validate(pizzaId => pizzas.Where(x => x.Id == pizzaId).FirstOrDefault() == null
+                                ? ValidationResult.Error("[red]Invalid input[/]")
+                                : ValidationResult.Success()));
 
-                        var getPizza = pizzaRepository.GetPizzaById(choosingPizza);
-
-                        var pizzaWithToppings = new PizzaWithTopping()
+                        var pizzaWithToppings = new PizzaWithToppingsModel
                         {
-                            Id = getPizza.Id,
-                            ToppingList = new List<int>()
+                            Id = pizzas.Where(x => x.Id == pizzaId).FirstOrDefault().Id
                         };
 
-                        order.TotalCost += getPizza.Price;
+                        order.TotalCost += pizzas.Where(x => x.Id == pizzaId).FirstOrDefault().Price;
 
                         while (true)
                         {
                             //Adding topping
                             var addTopping = AnsiConsole.Confirm("Do you want to add any topping on your pizza? y for [green]Yes[/], n for [red]No[/]");
+
                             if (addTopping)
                             {
-                                ToppingRepository.DisplayToppingTable(toppingRepository.ToppingList);
+                                ToppingRepository.DisplayToppingTable(toppings);
 
-                                var chooseTopping = AnsiConsole.Prompt(
-                                    new TextPrompt<int>("Please Choose your extra topping.")
-                                        .Validate(toppingId =>
-                                            toppingRepository.GetToppingById(toppingId) == null
-                                                ? ValidationResult.Error("[red]Invalid input[/]")
-                                                : ValidationResult.Success()));
+                                var chooseTopping = AnsiConsole.Prompt(new TextPrompt<int>("Please Choose your extra topping.")
+                                    .Validate(toppingId => toppings.Where(x => x.Id == toppingId).FirstOrDefault() == null
+                                        ? ValidationResult.Error("[red]Invalid input[/]")
+                                        : ValidationResult.Success()));
 
-                                var getTopping = toppingRepository.GetToppingById(chooseTopping);
+                                pizzaWithToppings.ToppingList.Add(new ToppingModel {Id = chooseTopping, PizzaId = index});
 
-                                pizzaWithToppings.ToppingList.Add(getTopping.Id);
-                                order.TotalCost += getTopping.Price;
-                                order.Toppings.Add(getTopping);
+                                order.TotalCost += toppings.Where(x => x.Id == chooseTopping).FirstOrDefault().Price;
                             }
                             else
                             {
-                                order.Pizzas.Add(pizzaWithToppings);
                                 break;
                             }
                         }
-                        
+
+                        order.Pizzas.Add(pizzaWithToppings);
                         AnsiConsole.WriteLine($"Pizza count => {order.Pizzas.Count}");
 
                         if (AnsiConsole.Confirm("Do you want to add another pizza? y for [green]Yes[/] n for [red]No[/]"))
+                        {
+                            index++;
                             continue;
-                        else
-                            break;
-                        
+                        }
+
+                        break;
                     }
-                    
-                    var note = AnsiConsole.Confirm("Do you want to leave any notes to the chef? y for [green]Yes[/] n for [red]No[/]");
-                    if (note)
+
+                    var addNote = AnsiConsole.Confirm("Do you want to leave any notes to the chef? y for [green]Yes[/] n for [red]No[/]");
+
+                    if (addNote)
                     {
-                        var typingNote = AnsiConsole.Prompt(
-                            new TextPrompt<string>("Leave your note here")
-                                .Validate(input => input == null
-                                    ? ValidationResult.Error("[Orange]please leave your note here[/]")
-                                    : ValidationResult.Success()));
-                        
-                        order.Note = typingNote;
+                        var note = AnsiConsole.Prompt(new TextPrompt<string>("Leave your note here")
+                            .Validate(input => input == null
+                                ? ValidationResult.Error("[Orange]please leave your note here[/]")
+                                : ValidationResult.Success()));
+
+                        order.Note = note;
                     }
-                    
-                    await orderRepository.PostOrderToApi(order);
+
+                    await OrderRepository.PostOrderToApi(order);
+                    break;
                 }
 
-                else if (answer == 2)
+                case 2:
                 {
                     //Viewing order list
-                    await orderRepository.GetOrdersFromApi();
-                    foreach (var o in orderRepository.OrderList)
-                        OrderRepository.DisplayOrderTable(o, pizzaRepository.PizzaList.ToList(),
-                            toppingRepository.ToppingList.ToList());
+                    var orders = await OrderRepository.GetOrdersFromApi();
+
+                    if (orders.Count == 0)
+                    {
+                        AnsiConsole.WriteLine("You don't have any orders yet!");
+                    }
+                    else
+                    {
+                        foreach (var o in orders)
+                            OrderRepository.DisplayOrderTable(o);
+                    }
+
+                    break;
                 }
 
-                else if (answer == 0)
+                case 0:
                 {
                     //Good bye
                     AnsiConsole.WriteLine("Good Bye!");
                     break;
                 }
             }
+
+            break;
         }
     }
 }
